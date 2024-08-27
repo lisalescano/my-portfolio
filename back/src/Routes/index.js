8ca62252f5d29498effb86060958dbb1cbe3aee0
lisalescano <lisalescano10@gmail.com>
2024-08-27 19:30:34 -0300

    fix: routes

--- back/src/Routes/index.js
+++ back/src/Routes/index.js
@@ -1,6 +1,10 @@
-const {Router} = require("express")
+const {Router} = require("express");
+const { getAllTechs, postNewTech } = require("../handlers/techHandlers");
 
 const router = Router()
 
+router.get('/technologies', getAllTechs)
+
+router.post('/technologies', postNewTech)
 
 module.exports = router;
